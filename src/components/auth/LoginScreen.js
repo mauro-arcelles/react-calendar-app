fc688970f554bed9b884e9640ef91a9fff4d83e4
mauroar1001 <83997911+mauro-arcelles@users.noreply.github.com>
2022-04-16 18:30:33 -0500

    Data por defecto en login screen eliminada

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -9,15 +9,15 @@ export const LoginScreen = () => {
   const dispatch = useDispatch();
 
   const [formLoginValues, handleLoginInputChange] = useForm({
-    lEmail: 'adrian@gmail.com',
-    lPassword: '123456'
+    lEmail: '',
+    lPassword: ''
   });
 
   const [formRegisterValues, handleRegisterInputChange] = useForm({
-    rName: 'Nando',
-    rEmail: 'mauro@gmail.com',
-    rPassword1: '123456',
-    rPassword2: '123456'
+    rName: '',
+    rEmail: '',
+    rPassword1: '',
+    rPassword2: ''
   });
 
   const { lEmail, lPassword } = formLoginValues;
